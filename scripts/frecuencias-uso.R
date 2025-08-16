6165c2d2b6189da661ab1e7f166526c1c785c657
JJ Merelo <jjmerelo@gmail.com>
2025-08-16 12:43:34 +0200

    #5 processing script

---
+++ scripts/frecuencias-uso.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(dplyr)
+
+frecuencias_data <- read.csv("data/frecuencias.csv", header = TRUE, sep = ";")
+
+porcentajes_data <-  frecuencias_data %>% group_by(Disciplina,Frecuencia) %>%
+  summarise(Número = n()) %>%
+  mutate(Proporción = Número / sum(Número))
+
+# reorder the levels of 'Frecuencia' for better visualization first none, then Nunca, then Casi nunca, then A veces, then Casi Siempre, then Siempre
+porcentajes_data$Frecuencia <- factor(porcentajes_data$Frecuencia,
+                                      levels = c("", "Nunca", "Casi nunca", "A veces", "Casi siempre", "Siempre"))
+
+ggplot(porcentajes_data, aes(x=Frecuencia, fill=Disciplina, y = Proporción)) +
+  geom_bar( stat="identity", position="dodge") +
+  labs(title="Frecuencia de uso", x="Frecuencia", y="Proporción") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("figures/frecuencias.png", width = 10, height = 6)
+
+frecuencia_tabla <- table( frecuencias_data$Frecuencia, frecuencias_data$Disciplina)
+chisq_frecuencia <- chisq.test(frecuencia_tabla)
