44ddddb9d847d6c1c908b823c8bc14522ea6f113
JJ Merelo <jjmerelo@gmail.com>
2025-08-16 18:28:39 +0200

    #5 script with :bug:

--- scripts/frecuencias-uso.R
+++ scripts/frecuencias-uso.R
@@ -3,25 +3,26 @@ library(dplyr)
 
 frecuencias_data <- read.csv("data/frecuencias-uso.csv", header = TRUE, sep = ";")
 
-# SPLIT administrativas;burocracia;calendario;comunicacion-profesorado;feedback;generar-material;mejorar-calidad;preguntas-temario;preparar-examen;profesor-particular;resumen;traduccion by ";"
 
 columnas <- strsplit("administrativas;burocracia;calendario;comunicacion-profesorado;feedback;generar-material;mejorar-calidad;preguntas-temario;preparar-examen;profesor-particular;resumen;traduccion", ";")
 
-for ( i in columnas ) {
-  # create a dataframe with the discipline and the content of the column i
+for ( i in columnas[[1]] ) {
+  # Change _ in i to .
+  i <- gsub("-", ".", i)
+  cat("Procesando disciplina:", i, "\n")
   frecuencias_uso_data  <-  data.frame(Disciplina = frecuencias_data$Disciplina,
-                                       Frecuencia = frecuencias_data[i])
+                                       frecuencias_data[i])
 
-  porcentajes_data <-  frecuencias_uso_data %>% group_by(Disciplina,Frecuencia) %>%
+  porcentajes_data <-  frecuencias_uso_data %>% group_by(Disciplina,.data[[i]]) %>%
   summarise(Número = n()) %>%
   mutate(Proporción = Número / sum(Número))
 
-  porcentajes_data$Frecuencia <- factor(porcentajes_data$Frecuencia,
+  porcentajes_data$Frecuencia <- factor(porcentajes_data[[i]],
                                       levels = c("No me parece útil", "Me gustaría hacerlo", "Lo he hecho"))
 
   ggplot(porcentajes_data, aes(x=Frecuencia, fill=Disciplina, y = Proporción)) +
     geom_bar( stat="identity", position="dodge") +
-    labs(title="Frecuencia de uso", x="Frecuencia", y="Proporción") +
+    labs(title=paste0("Frecuencia de uso ",i), x="Frecuencia", y="Proporción") +
     theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
   ggsave(paste0("frecuencias-uso-",i, ".png"), width = 10, height = 6)
