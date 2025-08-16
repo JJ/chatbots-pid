f904191d32ec05d160cd81e16aefac9754efe85b
JJ Merelo <jjmerelo@gmail.com>
2025-08-16 13:00:54 +0200

    #5 process frecuencies

--- scripts/frecuencias-uso.R
+++ scripts/frecuencias-uso.R
@@ -1,22 +1,30 @@
 library(ggplot2)
 library(dplyr)
 
-frecuencias_data <- read.csv("data/frecuencias.csv", header = TRUE, sep = ";")
+frecuencias_data <- read.csv("data/frecuencias-uso.csv", header = TRUE, sep = ";")
 
-porcentajes_data <-  frecuencias_data %>% group_by(Disciplina,Frecuencia) %>%
+# SPLIT administrativas;burocracia;calendario;comunicacion-profesorado;feedback;generar-material;mejorar-calidad;preguntas-temario;preparar-examen;profesor-particular;resumen;traduccion by ";"
+
+columnas <- strsplit("administrativas;burocracia;calendario;comunicacion-profesorado;feedback;generar-material;mejorar-calidad;preguntas-temario;preparar-examen;profesor-particular;resumen;traduccion", ";")
+
+for ( i in columnas ) {
+  # create a dataframe with the discipline and the content of the column i
+  frecuencias_uso_data  <-  data.frame(Disciplina = frecuencias_data$Disciplina,
+                                       Frecuencia = frecuencias_data[i])
+
+  porcentajes_data <-  frecuencias_uso_data %>% group_by(Disciplina,Frecuencia) %>%
   summarise(Número = n()) %>%
   mutate(Proporción = Número / sum(Número))
 
-# reorder the levels of 'Frecuencia' for better visualization first none, then Nunca, then Casi nunca, then A veces, then Casi Siempre, then Siempre
-porcentajes_data$Frecuencia <- factor(porcentajes_data$Frecuencia,
-                                      levels = c("", "Nunca", "Casi nunca", "A veces", "Casi siempre", "Siempre"))
+  porcentajes_data$Frecuencia <- factor(porcentajes_data$Frecuencia,
+                                      levels = c("No me parece útil", "Me gustaría hacerlo", "Lo he hecho"))
 
-ggplot(porcentajes_data, aes(x=Frecuencia, fill=Disciplina, y = Proporción)) +
-  geom_bar( stat="identity", position="dodge") +
-  labs(title="Frecuencia de uso", x="Frecuencia", y="Proporción") +
-  theme_minimal() +
+  ggplot(porcentajes_data, aes(x=Frecuencia, fill=Disciplina, y = Proporción)) +
+    geom_bar( stat="identity", position="dodge") +
+    labs(title="Frecuencia de uso", x="Frecuencia", y="Proporción") +
+    theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("figures/frecuencias.png", width = 10, height = 6)
-
+  ggsave(paste0("frecuencias-uso-",i, ".png"), width = 10, height = 6)
+}
 frecuencia_tabla <- table( frecuencias_data$Frecuencia, frecuencias_data$Disciplina)
 chisq_frecuencia <- chisq.test(frecuencia_tabla)
