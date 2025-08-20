74405d87eab20e85f86d99ed42a4a03231312851
JJ Merelo <jjmerelo@gmail.com>
2025-08-20 18:14:50 +0200

    closes #11 analizando los chatbots

---
+++ scripts/frecuencias-chatbots.R
@@ -0,0 +1,39 @@
+library(ggplot2)
+library(dplyr)
+
+frecuencias_data <- read.csv("data/chatbots.csv", header = TRUE, na.strings="", sep = ";")
+
+columnas <- strsplit("ChatGPT;Claude;Copilot;DeepSeek;Disciplina;Gemini;NotebookLM;Perplexity", ";")
+
+for ( i in columnas[[1]] ) {
+  # Change _ in i to .
+  i <- gsub("-", ".", i)
+  frecuencias_uso_data  <-  data.frame(Disciplina = frecuencias_data$Disciplina,
+                                       frecuencias_data[i])
+
+  porcentajes_data <-  frecuencias_uso_data %>% group_by(Disciplina,.data[[i]]) %>%
+    summarise(Número = n()) %>%
+  mutate(Proporción = Número / sum(Número))
+
+  porcentajes_data$Frecuencia <- factor(porcentajes_data[[i]],
+                                      levels = c("No lo conozco",
+                                                 "Lo conozco, pero no lo he usado",
+                                                 "Lo uso en el contexto universitario",
+                                                 "Lo uso tanto en el conexto universitario como para otras tareas",
+                                                 "Lo uso para otras tareas" )
+                                      )
+
+  ggplot(porcentajes_data, aes(x=Frecuencia, fill=Disciplina, y = Proporción)) +
+    geom_bar( stat="identity", position="dodge") +
+    labs(title=paste0("Actitud sobre ",i), x="Frecuencia", y="Proporción") +
+    theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  ggsave(paste0("figures/actitudes-",i, ".png"), width = 10, height = 6)
+
+  frecuencia_tabla <- table( unname(unlist(frecuencias_data[i])), frecuencias_data$Disciplina)
+  chisq_frecuencia <- chisq.test(frecuencia_tabla)
+  if ( chisq_frecuencia$p.value < 0.05 ) {
+    cat("✅ La prueba de chi-cuadrado indica que hay una diferencia significativa en la actitud 📈", i, "📈 con p-value ", chisq_frecuencia$p.value,"\n")
+  }
+}
+
