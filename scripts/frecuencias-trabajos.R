bd20eded9bab964bc450bb63c70abb74a4218899
JJ Merelo <jjmerelo@gmail.com>
2025-11-18 11:10:51 +0100

    #13 por género

--- scripts/frecuencias-trabajos.R
+++ scripts/frecuencias-trabajos.R
@@ -14,12 +14,32 @@ porcentajes_data$Disciplina <- factor(porcentajes_data$Disciplina,
 porcentajes_data$Mejora.de.Trabajos <- factor(porcentajes_data$Mejora.de.Trabajos,
                                       levels = c("Lo he hecho","Me gustaría hacerlo","No me parece útil"))
 
-ggplot(porcentajes_data, aes(x=Frecuencia, fill=Disciplina, y = Proporción)) +
+ggplot(porcentajes_data, aes(x=Mejora.de.Trabajos, fill=Disciplina, y = Proporción)) +
   geom_bar( stat="identity", position="dodge") +
   labs(title="Frecuencia de uso", x="Frecuencia", y="Proporción") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggsave("figures/frecuencias.png", width = 10, height = 6)
+ggsave("figures/mejora-trabajos.png", width = 10, height = 6)
 
-frecuencia_tabla <- table( frecuencias_data$Frecuencia, frecuencias_data$Disciplina)
+frecuencia_tabla <- table( frecuencias_data$Mejora.de.Trabajos, frecuencias_data$Disciplina)
 chisq_frecuencia <- chisq.test(frecuencia_tabla)
+
+genero_summary <-  frecuencias_data %>% group_by(Género,Mejora.de.Trabajos) %>%
+  summarise(Número = n()) %>%
+  mutate(Proporción = Número / sum(Número))
+genero_summary$Género <- factor(genero_summary$Género,
+                                      levels = c("Masculino",
+                                                 "Femenino",
+                                                 "Prefiero no decirlo")
+                                      )
+genero_summary$Mejora.de.Trabajos <- factor(genero_summary$Mejora.de.Trabajos,
+                                      levels = c("Lo he hecho","Me gustaría hacerlo","No me parece útil" ))
+
+ggplot(genero_summary, aes(x=Mejora.de.Trabajos, fill=Género, y = Proporción)) +
+  geom_bar( stat="identity", position="dodge") +
+  labs(title="Frecuencia de uso por género", x="Frecuencia", y="Proporción") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ggsave("figures/mejora-trabajos-genero.png", width = 10, height = 6)
+frecuencia_tabla_genero <- table( frecuencias_data$Mejora.de.Trabajos, frecuencias_data$Género)
+chisq_frecuencia_genero <- chisq.test(frecuencia_tabla_genero)
